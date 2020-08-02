bf4881f732468c262c0421acdfe1c11309437ae9
dianaccanelon <dianaccanelon@gmail.com>
2020-08-02 12:27:48 -0400

    adding footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='main-footer'>
-            <div className='footer-left'>Designed by ADJJ</div>
+            <div className='footer-left'>Designed by ADJJ </div>
             <div className='footer-right'>
                 <div className='mission-head'>OUR MISSION</div>
                 <div className='mission'>To help our community by making it easier & safer to enjoy our days with our furry friends.</div>
