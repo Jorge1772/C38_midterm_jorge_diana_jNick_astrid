b00c8b473bb4c11577b8420420fb71fda2a183b0
Astrid Massuh <astridmassuh@MacBook-Pro.local>
2020-08-02 15:19:47 -0400

    umcomment lines 28-35

--- server.js
+++ server.js
@@ -25,14 +25,14 @@ app.get('/api/places', (req, res) =>{
 });
 // END DEMO
 
-// if (process.env.NODE_ENV === 'production') {
-//   // Serve any static files
-//   app.use(express.static(path.join(__dirname, 'client/build')));
-//   // Handle React routing, return all requests to React app
-//   app.get('*', (request, response) => {
-//     response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-//   });
-// }
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
